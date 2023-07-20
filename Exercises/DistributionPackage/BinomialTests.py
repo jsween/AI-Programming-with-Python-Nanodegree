4f230bdbc6d72f32d54f9c759d025142563b70ac
Jon Sweeney <jonsweeney26@gmail.com>
2023-07-20 12:41:48 -0700

    test: add mean and stdev tests

--- Exercises/DistributionPackage/BinomialTests.py
+++ Exercises/DistributionPackage/BinomialTests.py
@@ -9,6 +9,12 @@ class TestBinomialClass(unittest.TestCase):
     def test_initialization(self):
         self.assertEqual(self.binomial.p, 0.5, 'incorrect default probability')
         self.assertEqual(self.binomial.n, 20, 'incorrect default number of trials')
+    
+    def test_calculatemean(self):
+        self.assertEqual(self.binomial.calculate_mean(), 10, 'incorrect mean')
+
+    def test_stdev(self):
+        self.assertEqual(round(self.binomial.calculate_stdev(), 2), 2.24, 'incorrect stdev')
 
 if __name__ == '__main__':
     unittest.main()
