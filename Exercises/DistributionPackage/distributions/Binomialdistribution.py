3e4cdeb6b57b3b31f77392dd0226db6b201ed518
Jon Sweeney <jonsweeney26@gmail.com>
2023-07-20 12:45:55 -0700

    feat: finish binomial dist

--- Exercises/DistributionPackage/distributions/Binomialdistribution.py
+++ Exercises/DistributionPackage/distributions/Binomialdistribution.py
@@ -71,6 +71,7 @@ class Binomial(Distribution):
 
         return self.p, self.n
     
+
     def plot_bar(self):
         """ Function to output a histogram of the instance variable data using 
         matplotlib pyplot library.
@@ -87,3 +88,92 @@ class Binomial(Distribution):
         plt.title('Bar Chart of Data')
         plt.xlabel('outcome')
         plt.ylabel('count')
+
+
+        def pdf(self, k):
+            """ Probability density function calculator for the binomial distribution.
+            
+            Args:
+                x (float): point for calculating the probability density function
+                
+            
+            Returns:
+                float: probability density function output
+
+            """
+            
+            a = math.factorial(self.n) / (math.factorial(k) * (math.factorial(self.n - k)))
+            b = (self.p ** k) * (1 - self.p) ** (self.n - k)
+            
+            return a * b
+        
+
+        def plot_bar_pdf(self):
+            """ Function to plot the pdf of the binomial distribution
+            
+            Args:
+                None
+            
+            Returns:
+                list: x values for the pdf plot
+                list: y values for the pdf plot
+                
+            """
+            
+            x = []
+            y = []
+            
+            # calculate the x values to visualize
+            for i in range(self.n + 1):
+                x.append(i)
+                y.append(self.pdf(i))
+
+            # make the plots
+            plt.bar(x, y)
+            plt.title('Distribution of Outcomes')
+            plt.ylabel('Probability')
+            plt.xlabel('Outcome')
+
+            plt.show()
+
+            return x, y         
+        
+
+    def __add__(self, other):
+        """ Function to add together two Binomial distributions with equal p
+        
+        Args:
+            other (Binomial): Binomial instance
+            
+        Returns:
+            Binomial: Binomial distribution
+            
+        """
+        
+        try:
+            assert self.p == other.p, 'p values are not equal'
+        except AssertionError as error:
+            raise
+        
+        result = Binomial()
+        result.n = self.n + other.n
+        result.p = self.p
+        result.calculate_mean()
+        result.calculate_stdev()
+        
+        return result
+        
+        
+    def __repr__(self):
+        """ Function to output the characteristics of the Binomial instance
+        
+        Args:
+            None
+        
+        Returns:
+            string: characteristics of the Gaussian
+        
+        """
+        
+        return "mean {}, standard deviation {}, p {}, n {}".\
+        format(self.mean, self.stdev, self.p, self.n)
