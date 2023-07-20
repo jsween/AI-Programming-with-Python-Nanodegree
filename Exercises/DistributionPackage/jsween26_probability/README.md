7eb5cefb61d84d1b9ac106d8cbc62eea52a51855
Jon Sweeney <jonsweeney26@gmail.com>
2023-07-20 13:10:05 -0700

    feat: setup for pypi

---
+++ Exercises/DistributionPackage/jsween26_probability/README.md
@@ -0,0 +1,15 @@
+# jsween26_probability package
+
+A simple probability packaged used to complete tasks for Udacity's AI Programming with Python Nanodegree.
+
+# Files
+
+Generaldistribution.py - General Distribution Class
+
+Binomialdistribution.py - Binomial Distribution Class
+
+Gaussiandistribution.py - Gaussian Distribtuion Class
+
+# Installation
+
+`pip install jsween26_probability`
--- Exercises/DistributionPackage/setup.py
+++ Exercises/DistributionPackage/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
-      version='0.2',
+setup(name='jsween26_probability',
+      version='0.0.1',
       description='Gaussian and Binomial distributions',
-      packages=['distributions'],
+      packages=['jsween26_probability'],
+      author='Jonathan Sweeney',
+      author_email='jonsweeney26@gmail.com',
       zip_safe=False)
