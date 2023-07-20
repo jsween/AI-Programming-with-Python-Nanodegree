1e4057535632c9b06fa92bc3133f8033c5962b8e
Jon Sweeney <jonsweeney26@gmail.com>
2023-07-20 10:42:55 -0700

    feat: add example code file

---
+++ Exercises/DistributionPackage/example_code.py
@@ -0,0 +1,10 @@
+from Gaussiandistribution import Gaussian
+
+gaussian_one = Gaussian(22, 2)
+print(gaussian_one)
+
+gaussian_two = Gaussian(200, 3)
+print(gaussian_two)
+
+gaussian_sum = gaussian_one + gaussian_two
+print(gaussian_sum)
