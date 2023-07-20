a3f0ca879249931d3b96abb6e3a2e448ba617ea9
Jon Sweeney <jonsweeney26@gmail.com>
2023-07-20 08:21:30 -0700

    tests: add test for std dev

--- Exercises/GaussianTests.py
+++ Exercises/GaussianTests.py
@@ -14,16 +14,16 @@ class TestGaussianClass(unittest.TestCase):
     #     self.assertEqual(round(self.gaussian.pdf(25), 5), 0.19947,\
     #      'pdf function does not give expected result') 
 
-    # def test_meancalculation(self):
-    #     self.gaussian.read_data_file('TestData/numbers.txt', True)
-    #     self.assertEqual(self.gaussian.calculate_mean(),\
-    #      sum(self.gaussian.data) / float(len(self.gaussian.data)), 'calculated mean not as expected')
+    def test_meancalculation(self):
+        self.gaussian.read_data_file('TestData/numbers.txt', True)
+        self.assertEqual(self.gaussian.calculate_mean(),\
+         sum(self.gaussian.data) / float(len(self.gaussian.data)), 'calculated mean not as expected')
 
-    # def test_stdevcalculation(self):
-    #     self.gaussian.read_data_file('TestData/numbers.txt', True)
-    #     self.assertEqual(round(self.gaussian.stdev, 2), 92.87, 'sample standard deviation incorrect')
-    #     self.gaussian.read_data_file('TestData/numbers.txt', False)
-    #     self.assertEqual(round(self.gaussian.stdev, 2), 88.55, 'population standard deviation incorrect')
+    def test_stdevcalculation(self):
+        self.gaussian.read_data_file('TestData/numbers.txt', True)
+        self.assertEqual(round(self.gaussian.stdev, 2), 92.87, 'sample standard deviation incorrect')
+        self.gaussian.read_data_file('TestData/numbers.txt', False)
+        self.assertEqual(round(self.gaussian.stdev, 2), 88.55, 'population standard deviation incorrect')
                 
     
 if __name__ == '__main__':
