a4636f779ecbabecb4ba487191fe2ba147a2f533
Jon Sweeney <jonsweeney26@gmail.com>
2023-08-10 15:36:01 -0700

    docs: add docstring

--- Project-2-Image-Classifier/get_data.py
+++ Project-2-Image-Classifier/get_data.py
@@ -21,21 +21,14 @@ def get_train_input_args():
     argparse module to created and defined these command line arguments. If
     the user fails to provide all of the arguments, then the default values are
     used for the missing arguments.
-    Command Line Arguments:
-      1. dir : image data directory
-      2. save : directory to save checkpoints
-      3. arch : Pytorch model
-      4. learning_rate : learning rate
-      5. hidden_units : number of hidden units
-      6. epochs : number of epochs
-      7. gpu : use GPU for training
+
     Parameters:
      None - simply using argparse module to create & store command line arguments
     Returns:
      parse_args() -data structure that stores the command line arguments object
     """
     print("Parsing arguments...")
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Parser for train command line arguments ")
     parser.add_argument(
         "dir",
         type=str,
@@ -83,6 +76,18 @@ def get_train_input_args():
 
 
 def get_predict_input_args():
+    """
+    Retrieves and parses 1 to 6 command line arguments provided by the user when
+    they run the program from a terminal window. This function uses Python's
+    argparse module to created and defined these command line arguments. If
+    the user fails to provide all of the arguments, then the default values are
+    used for the missing arguments.
+    
+    Parameters:
+     None - simply using argparse module to create & store command line arguments
+    Returns:
+     parse_args() -data structure that stores the command line arguments object
+    """
     print('Parsing command line arguments...')
     parser = argparse.ArgumentParser(description="Parser for prediction command line arguments")
     parser.add_argument(
