ba095661fd4d619e22afd5b6a60e0a1ffadd9194
Jon Sweeney <jonsweeney26@gmail.com>
2023-08-10 20:52:25 -0700

    docs: add docstrings

--- Project-2-Image-Classifier/classifier.py
+++ Project-2-Image-Classifier/classifier.py
@@ -21,10 +21,13 @@ def build_classifier(arch, hidden_units):
     """
     Builds the classifier model object
 
+    Parameters: 
+        arch: pytorch model
+        hidden_units: number of hidden units
     Returns:
         The classifier model
     """
-    print('Building the classifier...')
+    print("Building the classifier...")
     if arch == "vgg" or arch == "vgg19":
         model = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
     elif arch == "vgg16":
@@ -54,18 +57,18 @@ def build_classifier(arch, hidden_units):
     )
 
     model.classifier = classifier
-    print('Model is built.')
+    print("Model is built.")
 
     return model
 
 
-def check_validation_set(model, valid_loader, device='cpu'):    
-    '''
-    Calculate the number correct 
+def check_validation_set(model, valid_loader, device="cpu"):
+    """
+    Calculate the number correct
 
     Returns:
         Percentage correct divided by total
-    '''
+    """
     correct = 0
     total = 0
 
@@ -77,23 +80,24 @@ def check_validation_set(model, valid_loader, device='cpu'):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    return correct / total 
+    return correct / total
 
 
-def train_classifier(model, trainloader, validloader, epochs, print_every, learning_rate, device):
-    '''
+def train_classifier(
+    model, trainloader, validloader, epochs, print_every, learning_rate, device
+):
+    """
     Trains the classifier
+
     Parameters:
-        model - 
-        trainloader - 
-        validloader - 
-        epochs - 
-        print_every -
-        criterion -
-        optimizer - 
-        device - 
-    '''
-    print('Training the classifier...')
+        model - pytorch model used
+        trainloader - training images data
+        validloader - validation images data
+        epochs - number of epochs
+        print_every - print every n rows
+        device - cpu or gpu device
+    """
+    print("Training the classifier...")
     epochs = epochs
     print_every = print_every
     steps = 0
@@ -120,33 +124,51 @@ def train_classifier(model, trainloader, validloader, epochs, print_every, learn
 
             if steps % print_every == 0:
                 valid_accuracy = check_validation_set(model, validloader, device)
-                print("Epoch: {}/{}... ".format(e+1, epochs),
-                      "Loss: {:.4f}".format(running_loss / print_every),
-                      "Validation Accuracy: {}".format(round(valid_accuracy, 4)))
+                print(
+                    "Epoch: {}/{}... ".format(e + 1, epochs),
+                    "Loss: {:.4f}".format(running_loss / print_every),
+                    "Validation Accuracy: {}".format(round(valid_accuracy, 4)),
+                )
 
                 running_loss = 0
     print("Training is Complete")
 
 
-def save_model(model, dir):
-    print('Saving the model...')
+def save_model(model, file):
+    """
+    Saves te classifier model
 
-    checkpoint = {'transfer_model': model.cpu(),
-                'input_size': 25088,
-                'output_size': 102,
-                'features': model.features,
-                'classifier': model.classifier,
-                'state_dict': model.state_dict()
-                }
+    Parameters:
+        model - pytorch model
+        file - file name to save the model to
+    """
+    print("Saving the model...")
 
-    torch.save(checkpoint, dir)
-    print('Saving is complete.')
+    checkpoint = {
+        "transfer_model": model.cpu(),
+        "input_size": 25088,
+        "output_size": 102,
+        "features": model.features,
+        "classifier": model.classifier,
+        "state_dict": model.state_dict(),
+    }
+
+    torch.save(checkpoint, file)
+    print(f"Model saved as {file}.")
 
 
-def predict(image_path, checkpoint, topk=5):
-    ''' Predict the class (or classes) of an image using a trained deep learning model.
-    '''
+def predict(image_path, checkpoint, top_k):
+    """
+    Predict the class (or classes) of an image using a trained deep learning model.
     
+    Parameters:
+        image_path: path to the image data
+        checkpoint: saved model to use
+        top_k: top k predictions
+    Returns:
+        The results of the model's prediction
+    """
+
     with torch.no_grad():
         image = process_image(image_path)
         image = torch.from_numpy(image)
@@ -154,12 +176,21 @@ def predict(image_path, checkpoint, topk=5):
         image = image.float()
         model, _ = load_checkpoint(checkpoint)
         outputs = model(image)
-        probs, classes = torch.exp(outputs).topk(topk)
+        probs, classes = torch.exp(outputs).topk(top_k)
         return probs[0].tolist(), classes[0].add(1).tolist()
 
 
 def display_prediction(image_path, model, cat_to_name):
+    """
+    Displays the Predictions
+    
+    Parameters:
+        image_path: path to the image data
+        model: saved model to use for classification
+        cat_to_name: JSON data with category to name mapping
+    Returns: 
+        None
+    """
     probs, classes = predict(image_path, model)
     plant_classes = [cat_to_name[str(cls)] + "({})".format(str(cls)) for cls in classes]
     print(plant_classes, probs)
-    
